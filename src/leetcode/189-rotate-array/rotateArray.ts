24df3310b787251dcd8db89dc50aaebc8a7bdd51
Gang Yang <qriousgabriel@gmail.com>
2021-10-30 20:33:21 +0100

    removed incorrect solution

--- src/leetcode/189-rotate-array/rotateArray.ts
+++ src/leetcode/189-rotate-array/rotateArray.ts
@@ -1,31 +1,16 @@
-// export const rotate = (nums: number[], k: number): void => {
-//   const reverse = (nums: number[], i: number, j: number): void => {
-//     while (i < j) {
-//       let tmp = nums[i]
-//       nums[i] = nums[j]
-//       nums[j] = tmp
-//       i++
-//       j--
-//     }
-//   }
-//   const n = nums.length
-//   const actualRotation = k % n
-//   reverse(nums, 0, n - actualRotation - 1)
-//   reverse(nums, n - actualRotation, n - 1)
-//   reverse(nums, 0, n - 1)
-// }
-
 export const rotate = (nums: number[], k: number): void => {
-  const head = []
+  const reverse = (nums: number[], i: number, j: number): void => {
+    while (i < j) {
+      let tmp = nums[i]
+      nums[i] = nums[j]
+      nums[j] = tmp
+      i++
+      j--
+    }
+  }
   const n = nums.length
-  const actualK = k % n
-  for (let i = actualK + 1; i < n; i++) {
-    head.push(nums[i])
-  }
-  for (let i = actualK + 1; i < n; i++) {
-    nums[i] = nums[i - actualK]
-  }
-  for (let i = actualK, j = 0; i < n; i++, j++) {
-    nums[i] = head[j]
-  }
+  const actualRotation = k % n
+  reverse(nums, 0, n - actualRotation - 1)
+  reverse(nums, n - actualRotation, n - 1)
+  reverse(nums, 0, n - 1)
 }
